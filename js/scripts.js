14c3a1c24d2c9fba50765ba6a672de690226bde1
Dagmara Jagodzińska <dag.jagodzinska@wp.pl>
2016-11-26 15:29:41 +0100

    poprawka

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,12 @@ var b = prompt('Podaj b');
 value = (a*a) + (2 * a * b) - (b*b);
 console.log('Wynik równy jest: ' + value);
 
-if (value>0) {
+if (value > 0) {
 	console.log('Wynik jest dodatni');
 }
-else if (value=0) {
+else if (value == 0) {
 	console.log('Wynik równa się 0');
 }
 else {
 	console.log('Wynik jest ujemny');
 }
-
